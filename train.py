fb6345523a1c0fa80e558e2d5c34a89960e3e5e4
Mustafa Eyceoz <maxusmusti@gmail.com>
2023-02-24 13:32:16 -0500

    Added env loop

---
+++ train.py
@@ -0,0 +1,16 @@
+from nes_py.wrappers import JoypadSpace
+import gym_tetris
+from gym_tetris.actions import MOVEMENT, SIMPLE_MOVEMENT
+
+env = gym_tetris.make('TetrisA-v0')
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+done = True
+for step in range(50000):
+    if done:
+        state = env.reset()
+    state, reward, done, info = env.step(env.action_space.sample())
+    #print(state)
+    env.render()
+
+env.close()
